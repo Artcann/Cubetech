a04a285f5799f4026fdd331804ffc7d5184d3089
paul-clgr <72009372+paul-clgr@users.noreply.github.com>
2020-11-24 12:59:06 +0100

    .

--- view/Cgu/index.php
+++ view/Cgu/index.php
@@ -1,33 +1,8 @@
 <?php
 
-#$this->title = "CGU";
+$this->title = "CGU";
 $this->style = "public/css/index.css";
 
-$db = parse_ini_file("C:\wamp64\www\Cubetech\Configuration\dev.ini");
 
-$user = $db['login'];
-$pass = $db['mdp'];
-$name = $db['dsn'];
-
-try{
-    $bdd = new PDO($name, $user, $pass);
-}
-catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
-
-
-
-$text = $bdd->query('SELECT contenu, titre FROM cgu');
-
-
-while ($donnees = $text->fetch())
-{
-    echo '<h2>'.$donnees['titre'].'</h2>';
-    echo $donnees['contenu'] . '<br />';
-}
-
-$text->closeCursor();
-
+print_r($data);
 ?>
