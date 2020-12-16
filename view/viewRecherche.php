475d8f08e704d42e395d2bd19336c656c7f17588
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2020-12-16 04:35:55 +0100

    Add files via upload

--- view/viewRecherche.php
+++ view/viewRecherche.php
@@ -1,43 +1,44 @@
-<?php
+*<?php
 
 $this->title = "Rechercher un militaire";
 $this->style = "public/css/styleR.css";
 ?>
 
 <?php include_once "view/header.html"; ?>
+<?php include_once "styleR.css"; ?>
 
 <div>
 	<h1 id="titre">Voici le résultat de votre recherche :<h1>
 <table cellspacing="20">
 	<tr>
-	<th>Nom</th>
-	<th>Pénom</th>
-	<th>Login</th>
-	<th>Mail</th>
-	<th>Date de naissance</th>
-	<th>Nationalité</th>
-	<th>Statut</th>
-	<th>Grade</th>
-	<th>Corps d'armée</th>
-	<th>Caserne de rattachement</th>
-	<th>Matricule</th>
+	<th class="texte">Nom</th>
+	<th class="texte">Pénom</th>
+	<th class="texte">Login</th>
+	<th class="texte">Mail</th>
+	<th class="texte">Date de naissance</th>
+	<th class="texte">Nationalité</th>
+	<th class="texte">Statut</th>
+	<th class="texte">Grade</th>
+	<th class="texte">Corps d'armée</th>
+	<th class="texte">Caserne de rattachement</th>
+	<th class="texte">Matricule</th>
 	</tr>
 	<tr>
 	<?php foreach($row=mysql_fetch_array($resultat) as $row)
 		{echo "<tr>
-		<th class="texte">".$row["nom"].</th>
-		<th class="texte">".$row["nom"].</th>
-		<th class="texte">".$row["prenom"].</th>
-		<th class="texte">".$row["login"].</th>
-		<th class="texte">".$row["mail"].</th>
-		<th class="texte">".$row["naissance"].</th>
-		<th class="texte">".$row["nationalite"].</th>
-		<th class="texte">".$row["statut"].</th>
-		<th class="texte">".$row["grade"].</th>
-		<th class="texte">".$row["corps"].</th>
-		<th class="texte">".$row["caserne"].</th>
-		<th class="texte">".$row["matricule"].</th>
-		       </tr>";}
+		<th>.$row["nom"].</th>
+		<th>.$row["prenom"].</th>
+		<th>.$row["login"].</th>
+		<th>.$row["mail"].</th>
+		<th>.$row["naissance"].</th>
+		<th>.$row["nationalite"].</th>
+		<th>.$row["statut"].</th>
+		<th>.$row["grade"].</th>
+		<th>.$row["corps"].</th>
+		<th>.$row["caserne"].</th>
+		<th>.$row["matricule"].</th>
+		<th>.$row["matricule"].</th>
+			</tr>";}
 	?>
 	</tr>
 </table>
