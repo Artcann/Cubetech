7e71c2117247e37cbe49823ae7792bd6b7b224c4
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2020-12-16 04:44:40 +0100

    Update ControllerRecherche.php

--- controller/ControllerRecherche.php
+++ controller/ControllerRecherche.php
@@ -8,6 +8,7 @@ class ControllerRecherche extends Controller
 
 
 public function getUser()
+{
 if (isset($_GET['identifiant'])||$_GET['nom']||$_GET['prenom']||$_GET['login']||$_GET['naissance']||$_GET['mail']||$_GET['statut']||$_GET['grade']||$_GET['corps']||$_GET['matricule']||$_GET['nationalite'])) 
 {
     $resultat->query($db,$sql);
@@ -17,7 +18,7 @@ if (isset($_GET['identifiant'])||$_GET['nom']||$_GET['prenom']||$_GET['login']||
 }
 else 
 {echo 'Veuillez renseigner au moins 1 champ !';}
-
+}
 
 
 include('DeconnexionRecherche.php');/*déconexion à la db*/
