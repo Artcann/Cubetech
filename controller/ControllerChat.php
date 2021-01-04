133ae28190b436d3d8e4bdfb92c77cda409a7cfd
Artcann <art.cann@orange.fr>
2021-01-04 16:47:53 +0100

    Faq

--- controller/ControllerChat.php
+++ controller/ControllerChat.php
@@ -4,12 +4,6 @@ include_once 'Model/Chat.php';
 
 class ControllerChat extends Controller {
 
-    private $chat;
-
-    public function __construct() {
-        $this->chat = new Chat();
-    }
-
     public function index() {
         if(!$this->request->isParameterSet('id')) {
             http_response_code(404);
@@ -18,10 +12,5 @@ class ControllerChat extends Controller {
             $this->generateView();
         }
     }
-
-    public function insert() {
-        $this->chat->insertMessage(1, 2, "test");
-    }
-
     
 }
