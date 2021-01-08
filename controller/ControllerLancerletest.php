eb4e1d8663b9395b6ef48c761f7c2ac90eaa624c
ugo <72391421+ughyper3@users.noreply.github.com>
2021-01-08 15:27:56 +0100

    Lancement du test

---
+++ controller/ControllerLancerletest.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'Framework/Controller.php';
+include_once 'Model/Test.php';
+
+class ControllerLancerletest extends Controller {
+
+	private $test;
+
+    public function __construct() {
+        $this->test = new Test();
+    }
+
+    public function index()
+    {
+        $this->generateView();
+    }
+
+}
