3a93294a9af2b65b7342c7c41da162b71715bdd7
Artcann <art.cann@orange.fr>
2021-01-12 15:48:29 +0100

    Update ControllerRecherche.php

--- controller/ControllerRecherche.php
+++ controller/ControllerRecherche.php
@@ -7,28 +7,24 @@ require_once 'Framework/Controller.php';
 class ControllerRecherche extends Controller {
 
 
+    public function index()
 
- 
- public function index()
+    {
+        $id = $this->request->getParameter('identifiant');
+        $nom = $this->request->getParameter('nom');
+        $prenom = $this->request->getParameter('prénom');
+        $naissance = $this->request->getParameter('date');
+        $grade = $this->request->getParameter('grade');
+        $caserne = $this->request->getParameter('caserne');
+        $nationalite = $this->request->getParameter('nationalité');
+        $corps = $this->request->getParameter('corps');
+        $statut = $this->request->getParameter('statut');
+        $matricule = $this->request->getParameter('matricule');
+        $login = $this->request->getParameter('login');	
+  
+        $dataArr = $this->getAllUsers($id);
 
-{
-     
- $id = $this->request->getParameter('identifiant');
-	$nom = $this->request->getParameter('nom');
-	$prenom = $this->request->getParameter('prénom');
-	$naissance = $this->request->getParameter('date');
-	$grade = $this->request->getParameter('grade');
-	$caserne = $this->request->getParameter('caserne');
-	$nationalite = $this->request->getParameter('nationalité')
-	$corps = $this->request->getParameter('corps');
-	$statut = $this->request->getParameter('statut');
-	$matricule = $this->request->getParameter('matricule');
-	$login = $this->getParameter('login');	
-	        
-	
-	$dataArr = $this->getAllUsers($id);
-
-    $this->generateView(array("data" => $dataArr));
+        $this->generateView(array("data" => $dataArr));
 }
 
 }
