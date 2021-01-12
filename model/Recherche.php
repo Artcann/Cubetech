b72062b31665cdbff03a6ef7ada3f2129a05ee72
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2021-01-12 09:59:15 +0100

    Add files via upload

---
+++ model/Recherche.php
@@ -0,0 +1,22 @@
+/*model recherche*/
+<?php
+require_once("Framework/Model.php");
+
+class Recherche extends Model
+
+ public function getAllUsers($id, $nom, $prenom, $naissance, $grade, $caserne, $nationalite, $corps, $statut, $matricule, $login)
+{
+  if getParameter()
+        {
+        $sql = "SELECT nom,prenom,login,mail,naissance,nationalite,statut,grade,corps,caserne,matricule FROM user
+        NATURAL JOIN caserne 
+        WHERE (nom LIKE"%$nom%" XOR prenom LIKE"%$prenom%" XOR login ="$login" XOR naissance="$naissance" XOR nationalite="$nationalite" XOR statut="$statut" XOR grade="$grade"
+        XOR corps="$corps" XOR caserne="$caserne" XOR matricule="$matricule")
+        ORDER BY nom";
+
+        $response = $this->executeRequest($sql);
+
+        $dataArr = array($reponse);
+        return $dataArr;
+        }
+}
