4afd7e8926fc70df7a11ad6b3aa8f413e81be4a5
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2021-01-14 17:43:09 +0100

    Update form.php

--- view/Recherche/form.php
+++ view/Recherche/form.php
@@ -1,47 +1,53 @@
-/*vue résultat*/
 <?php
 $this->title='Page Recherche';
-$this->style='public/css/styleR.css';
+#$this->style='public/css/styleR.css';
 /*$this->script='recherche.js';*/
 ?>
 
+<div class="table-container">
 
-<table>
-<thead>
-<tr>
-    <td>Nom</td>
-    <td>Prenom</td>
-    <td>Login</td>
-    <td>Mail</td>
-    <td>Date de naissance</td>
-    <td>Nationalité</td>
-    <td>Statut</td>
-    <td>Grade</td>
-    <td>Corps d'armée</td>
-    <td>Caserne de rattachement</td>
-    <td>Matricule</td>
-</tr>    
-</thead>
+    <h1>Resultat de la recherche</h1>
 
-<tbody>
-<?php
-    foreach($dataArr as $k => $v): ?>
-        <tr>
-            <td><?php echo $v['nom']; ?></td>
-            <td><?php echo $v['prenom']; ?></td>
-            <td><?php echo $v['login']; ?></td>
-            <td><?php echo $v['mail']; ?></td>
-            <td><?php echo $v['naissance']; ?></td>
-            <td><?php echo $v['nationalite']; ?></td>
-            <td><?php echo $v['statut']; ?></td>
-            <td><?php echo $v['grade']; ?></td>
-            <td><?php echo $v['corps']; ?></td>
-            <td><?php echo $v['caserne']; ?></td>
-            <td><?php echo $v['matricule']; ?></td>
-        </tr>
-    <?php endforeach ?>
-    <?php if(empty($id,$nom,$prenom,$login,$naissance,$grade,$nationalite,$statut,$matricule,$corps,$caserne)) {
-        echo'<p>"Veuillez renseigner au moins 1 champ"</p>';}?>
-    <?php if($reponse==0) {echo '<p>Désolé, votre recherche n\'a rien donné</p>';}?>
-</tbody>
-</table>
+    <table>
+    <thead>
+    <tr>
+        <td>Avatar</td>
+        <td>Nom</td>
+        <td>Prenom</td>
+        <td>Login</td>
+        <td>Mail</td>
+        <td>Date de naissance</td>
+        <td>Nationalité</td>
+        <td>Statut</td>
+        <td>Grade</td>
+        <td>Corps d'armée</td>
+        <td>Caserne de rattachement</td>
+        <td>Matricule</td>
+        <td>Acceder aux resultats</td>
+        <td>Envoyer un message</td>
+    </tr>    
+    </thead>
+
+    <tbody>
+    <?php
+        foreach($data as $k => $v): ?>
+            <tr>
+                <td><img alt="" id="pict" src="public/avatars/<?php echo $v["login"];?>.jpg"></td>
+                <td><?php echo $v['nom']; ?></td>
+                <td><?php echo $v['prenom']; ?></td>
+                <td><?php echo $v['login']; ?></td>
+                <td><?php echo $v['mail']; ?></td>
+                <td><?php echo $v['naissance']; ?></td>
+                <td><?php echo $v['nationalite']; ?></td>
+                <td><?php echo $v['statut']; ?></td>
+                <td><?php echo $v['grade']; ?></td>
+                <td><?php echo $v['corps']; ?></td>
+                <td><?php echo $v['caserne']; ?></td>
+                <td><?php echo $v['matricule']; ?></td>
+                <td><a href="result/<?php echo $v["id"];?>"><img class="logo" src="public/images/graph.png"></a></td>
+                <td><a href="chat/<?php echo $v["id"];?>"><img class="logo" src="public/images/mail.png" data-alt-src="public/images/mail2.png"></a></td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+    </table>
+</div>
