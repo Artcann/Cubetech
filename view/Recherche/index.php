50984a196510dfa3c046c7474868d7ff97926edd
Yassine-94 <72301872+Yassine-94@users.noreply.github.com>
2021-01-18 15:03:45 +0100

    Update index.php

--- view/Recherche/index.php
+++ view/Recherche/index.php
@@ -1,25 +1,30 @@
 <?php
 $this->title='Formulaire page Recherche';
-#$this->style='public/css/styleRform.css';
-/*$this->script='recherche.js';*/
+$this->style='public/css/recherche.css';
 ?>
 
 <html>
-<body>
-<form method="post" action="recherche/result" id="formulaire">
+<body><br>
+<form method="post" action="recherche/result">
 <fieldset>
-    <legend>Veuillez remplir les champs souhaités:</legend>
-      <input type="text" name="identifiant" id="identifiant" value=""/><br>
-      <input type="text" name="nom" id="nom" value=""/><br>
-      <input type="text" name ="prenom" id="prenom" value=""/><br>
-      <input type="text" name="âge" id="date" value=""/><br>
-      <input type="text" name="caserne" id="caserne" value=""/><br>
-      <!-- TODO GRADE SELECT INPUT -->
-      <input type="text" name="nationalité" id="nationalité" value=""/><br>
-      <input type="text" name="corps militaire" id="corps" value=""/><br>
-      <input type="text" name="statut" id="statut" value=""/><br>
-      <input type="text" name="matricule" id="matricule" value=""/><br>
-      <input type="text" name="login" id="login" value=""/><br>
-      <button type="submit">Rechercher</button><button type="reset">Effacer</button>
+      <legend align="center" id="titre">Veuillez remplir les champs souhaités :</legend>
+      <input type="text" name="identifiant" id="identifiant" value="" placeholder="Identifiant"/><br><br>
+      <input type="text" name="nom" id="nom" value="" placeholder="Nom"/><br><br>
+      <input type="text" name ="prenom" id="prenom" value="" placeholder="Prénom"/><br><br>
+      <input type="text" name="âge" id="date" value="" placeholder="Date de naissance"/><br><br>
+      <input type="text" name="caserne" id="caserne" value="" placeholder="Caserne"/><br><br>
+      <select><option value=""><strong>----Selectionnez un grade----</strong></option>
+            <option value="général">Général</option>
+            <option value="capitaine">Capitaine</option>
+            <option value="lieutenant">Lieutenant</option>
+            <option value="sergent">Sergent</option>
+            <option value="soldat">Soldat</option></select><br><br>
+      <input type="text" name="nationalité" id="nationalité" value="" placeholder="Nationalité"/><br><br>
+      <input type="text" name="corps militaire" id="corps" value="" placeholder="Corps militaire"/><br><br>
+      <input type="text" name="statut" id="statut" value="" placeholder="Statut"/><br><br>
+      <input type="text" name="matricule" id="matricule" value="" placeholder="Matricule"/><br><br>
+      <input type="text" name="login" id="login" value="" placeholder="Login"/><br><br>
+      <button type="submit">Rechercher</button><button type="reset">Effacer</button><br>
+</fieldset>
 </body>
 </html>
