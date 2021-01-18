a293b567ba0d64973e09a8b81491f978a96a51ed
LaTrad <72390967+LaTrad@users.noreply.github.com>
2021-01-18 14:32:18 +0100

    home en fonction du statut

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,15 @@
 
-
-
-    $this.data('alt-src', $this.attr('src'));
-
-    $this.attr('src', newSource);
-}
-
-
-$(function () {
-    $('img.alert').hover(imageSwap, imageSwap);
-});
+//
+//
+//     $this.data('alt-src', $this.attr('src'));
+//
+//     $this.attr('src', newSource);
+// }
+//
+//
+// $(function () {
+//     $('img.alert').hover(imageSwap, imageSwap);
+// });
 
 
 
@@ -17,17 +17,14 @@ $(function () {
 
 if (statut === 'Administrateur'){
 
-    document.getElementById('button1').innerText = 'Accèder au back office';
+    // document.getElementById('button1').innerText = 'Accèder au back office';
     $("#button1").attr("href", "adminhome");
     document.getElementById('button2').style.display="none";
 
-    document.getElementById('agenda').innerText = '';
+    // document.getElementById('agenda').innerText = '';
 }
 
 
 else if (statut === 'Ressource humaine') {
-
-	document.getElementById('button1').innerText = 'Démarrer un test';
   $("#button1").attr("href", "lancerLeTest");
-  document.getElementById('button2').innerText= 'Voir mon agenda';
 }
